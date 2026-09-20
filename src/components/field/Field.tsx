1948b944fc15048b26b9934cb5dedf4f044447bd
Max <scorpiomax90@gmail.com>
2026-09-20 19:15:52 +0300

    added ts for field.tsx

--- src/components/field/Field.tsx
+++ src/components/field/Field.tsx
@@ -1,15 +1,26 @@
-const Field = (props) => {
-  const {
-    className = '',
-    id,
-    label,
-    type = 'text',
-    value,
-    onChange,
-    ref,
-    error,
-  } = props;
+import type { HTMLInputTypeAttribute, Ref } from 'react';
 
+interface FieldProps {
+  className?: string;
+  id: string;
+  label: string;
+  type?: HTMLInputTypeAttribute;
+  value?: string;
+  onChange?: (value: string) => void;
+  ref?: Ref<HTMLInputElement>;
+  error?: string;
+}
+
+const Field = ({
+  className = '',
+  id,
+  label,
+  type = 'text',
+  value,
+  onChange,
+  ref,
+  error,
+}: FieldProps) => {
   return (
     <div className={`field ${className}`}>
       <label className="field__label" htmlFor={id}>
@@ -22,7 +33,7 @@ const Field = (props) => {
         autoComplete="off"
         type={type}
         value={value}
-        onInput={(event) => onChange?.(event.target.value)}
+        onInput={(event) => onChange?.(event.currentTarget.value)}
         ref={ref}
       />
       {error && <span className="field__error">{error}</span>}
